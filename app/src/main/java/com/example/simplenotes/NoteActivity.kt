ea41697b47b6fe52a9212c1db636910700bd56ff
Iskandar Rassulov <iskander.rassulov.2002@gmail.com>
2024-05-11 21:41:28 +0200

    defined delete button

--- app/src/main/java/com/example/simplenotes/NoteActivity.kt
+++ app/src/main/java/com/example/simplenotes/NoteActivity.kt
@@ -1,14 +1,17 @@
 package com.example.simplenotes
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class NoteActivity : AppCompatActivity() {
 
     private lateinit var noteEditText: EditText
     private lateinit var saveNoteButton: Button
+    private lateinit var deleteNoteButton: Button
     private lateinit var cancelNoteButton: Button
     private var noteId: Int? = null
 
@@ -18,21 +21,25 @@ class NoteActivity : AppCompatActivity() {
 
         noteEditText = findViewById(R.id.noteEditText)
         saveNoteButton = findViewById(R.id.saveNoteButton)
+        deleteNoteButton = findViewById(R.id.deleteNoteButton)
         cancelNoteButton = findViewById(R.id.cancelNoteButton)
+
         val repository = Repository(this)
 
-        noteId = intent?.getIntExtra("noteId", -1)
-        if (noteId != null && noteId != -1) {
+        // Получение ID заметки, если она существует
+        noteId = intent.getIntExtra("noteId", -1)
+        if (noteId != -1) {
             val note = repository.loadNotes().find { it.id == noteId }
             note?.let {
                 noteEditText.setText(it.content)
+                deleteNoteButton.visibility = View.VISIBLE
             }
         }
 
         saveNoteButton.setOnClickListener {
             val content = noteEditText.text.toString()
             if (content.isNotBlank()) {
-                if (noteId != null && noteId != -1) {
+                if (noteId != -1) {
                     repository.saveNotes(repository.loadNotes().map {
                         if (it.id == noteId) it.copy(content = content) else it
                     })
@@ -42,6 +49,20 @@ class NoteActivity : AppCompatActivity() {
                 }
                 finish()
             }
+        }
+
+        deleteNoteButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Delete Note")
+                .setMessage("Are you sure you want to delete this note?")
+                .setPositiveButton("Yes") { dialog, which ->
+                    if (noteId != -1) {
+                        repository.deleteNote(noteId!!)
+                        finish()
+                    }
+                }
+                .setNegativeButton("No", null)
+                .show()
         }
 
         cancelNoteButton.setOnClickListener {
